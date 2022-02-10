f5a1f903f16e6ad2f35cdfce6aac5185942a2e51
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-02-10 14:30:41 +0100

    Add and handle flag to use IDTOKENS authentication.
    This adds a knob "use_idtokens_auth" to enable IDTOKENS auth.
    Still missing:
    - Add related configuration attributes / knobs.
    - Update docs.

--- lib/puppet/parser/functions/construct_auth_string.rb
+++ lib/puppet/parser/functions/construct_auth_string.rb
@@ -2,13 +2,14 @@ module Puppet::Parser::Functions
   # rubocop:disable Style/HashSyntax
   newfunction(:construct_auth_string, :type => :rvalue) do |args|
     # rubocop:enable Style/HashSyntax
-    raise(Puppet::ParseError, "construct_auth_string() wrong number of arguments. Given: #{args.size} for 6)") if args.size != 6
+    raise(Puppet::ParseError, "construct_auth_string() wrong number of arguments. Given: #{args.size} for 7)") if args.size != 7
     use_fs_auth = args[0]
     use_password_auth = args[1]
     use_kerberos_auth = args[2]
     use_claim_to_be_auth = args[3]
     use_anonymous_auth = args[4]
     use_ssl_auth = args[5]
+    use_idtokens_auth = args[6]
 
     fs_string = 'FS'
     pw_string = 'PASSWORD'
@@ -16,6 +17,7 @@ module Puppet::Parser::Functions
     ctb_string = 'CLAIMTOBE'
     anon_string = 'ANONYMOUS'
     ssl_string = 'SSL'
+    idtokens_string = 'IDTOKENS'
 
     auth_methods = []
     if use_fs_auth == true
@@ -35,6 +37,9 @@ module Puppet::Parser::Functions
     end
     if use_ssl_auth == true
       auth_methods.push ssl_string
+    end
+    if use_idtokens_auth == true
+      auth_methods.push idtokens_string
     end
 
     return auth_methods.join(',')
